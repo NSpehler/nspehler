eab88f2f5fd380cf6f39167d76f333b606a538d5
Nicolas Spehler <nicolas@spehler.com>
2021-11-08 22:07:35 +0800

    🔧 (terraform): Terraform configuration for Gatsby Cloud hosting

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -14,6 +14,34 @@ resource "cloudflare_zone_settings_override" "nspehler" {
   }
 }
 
+# Gatsby Cloud
+resource "cloudflare_record" "gatsby_cloud" {
+  zone_id = cloudflare_zone.nspehler.id
+  name    = local.domain
+  value   = "199.232.194.22"
+  type    = "A"
+  ttl     = "1"
+  proxied = "false"
+}
+
+resource "cloudflare_record" "gatsby_cloud_secondary" {
+  zone_id = cloudflare_zone.nspehler.id
+  name    = local.domain
+  value   = "199.232.198.22"
+  type    = "A"
+  ttl     = "1"
+  proxied = "false"
+}
+
+resource "cloudflare_record" "gatsby_cloud_www" {
+  zone_id = cloudflare_zone.nspehler.id
+  name    = "www"
+  value   = "nspehler.gatsbyjs.io"
+  type    = "CNAME"
+  ttl     = "1"
+  proxied = "false"
+}
+
 # DatoCMS
 resource "cloudflare_record" "datocms" {
   zone_id = cloudflare_zone.nspehler.id
