d746027aedadd94a09b716e2227a3156752d87aa
Nicolas Spehler <nicolas@spehler.com>
2021-11-24 16:58:24 +0800

    🔍 (client): Add sitemap, robots.txt and structured data

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -7,7 +7,20 @@ import { Layout } from "../components/Layout"
 
 const IndexPage = ({ data, location }) => (
   <Layout location={location}>
-    <HelmetDatoCms seo={data.home.seoMetaTags} />
+    <HelmetDatoCms seo={data.home.seoMetaTags}>
+      <script type="application/ld+json">
+        {JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          name: process.env.GATSBY_APP_NAME,
+          email: "nspehler@hey.com",
+          telephone: "+1 (415) 513-0099",
+          url: process.env.GATSBY_APP_URL,
+          logo: `${process.env.GATSBY_APP_URL}/nspehler-icon.png`,
+          sameAs: data.footer.social.map((social) => social.link),
+        })}
+      </script>
+    </HelmetDatoCms>
     <div className="prose prose-xl lg:prose-2xl dark:prose-light">
       <StructuredText data={data.home.content} />
     </div>
@@ -20,6 +33,9 @@ export const query = graphql`
   query HomeQuery {
     home: datoCmsHome {
       ...Home
+    }
+    footer: datoCmsFooter {
+      ...Footer
     }
   }
 `
