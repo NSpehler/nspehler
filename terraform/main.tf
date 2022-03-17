6ef927ecb1215ae6ff111ec1bc61d48e41c9fa91
Nicolas Spehler <nicolas@spehler.com>
2022-03-17 09:18:45 +0100

    🔧 (terraform): Switch to Terraform Cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,24 @@
 locals {
-  aws_region       = "eu-west-1"
-  aws_profile      = "default"
-  cloudflare_email = "nspehler@hey.com"
-  domain           = "nspehler.com"
+  domain = "nspehler.com"
 }
 
-provider "aws" {
-  region  = local.aws_region
-  profile = local.aws_profile
-}
+provider "aws" {}
 
 provider "aws" {
   region  = "us-east-1"
   alias   = "us-east-1"
-  profile = local.aws_profile
 }
 
 data "aws_caller_identity" "current" {}
 
-provider "cloudflare" {
-  email = local.cloudflare_email
-  api_key = var.cloudflare_api_key
-}
+provider "cloudflare" {}
 
 # Backup Terraform state in S3
 terraform {
-  backend "s3" {
-    bucket         = "nspehler-terraform"
-    dynamodb_table = "nspehler-terraform"
-    key            = "terraform.tfstate"
-    region         = "eu-west-1"
-    profile        = "default"
-    encrypt        = true
+  cloud {
+    organization = "nspehler"
+    workspaces {
+      name = "nspehler"
+    }
   }
 }
