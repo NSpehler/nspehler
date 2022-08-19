26c385d25ad61fe2f985e9c7dd47a27a9ef822ea
Nicolas Spehler <nicolas@spehler.com>
2022-08-19 17:20:30 +0800

    🔧 (terraform): Create S3 bucket for lambda layers

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -8,3 +8,8 @@ resource "aws_s3_bucket_public_access_block" "nspehler" {
   block_public_acls   = false
   block_public_policy = false
 }
+
+resource "aws_s3_bucket" "nspehler_layers" {
+  bucket = "nspehler-layers"
+  acl    = "private"
+}
