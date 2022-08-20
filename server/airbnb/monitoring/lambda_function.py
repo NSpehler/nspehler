2d6dfb154dc08eacabb4efbe74fee917ede25a4b
Nicolas Spehler <nicolas@spehler.com>
2022-08-20 16:31:13 +0800

    🥅 (server): Retry every 2 seconds if request fails

--- server/airbnb/monitoring/lambda_function.py
+++ server/airbnb/monitoring/lambda_function.py
@@ -1,7 +1,9 @@
 import os
 import json
+import time
 import cloudscraper
 
+from retrying import retry
 from pymongo import UpdateOne
 from bson.objectid import ObjectId
 
@@ -12,7 +14,7 @@ def lambda_handler(event, context):
     searches = get_searches()
     for search in searches:
         print(f"[{search['_id']}] Get listings")
-        listings = get_listings(variables=search["variables"])
+        listings = get_listings(search_id=search["_id"], variables=search["variables"])
         
         print(f"[{search['_id']}] Found {len(listings)} listings")
         upsert_listings(search_id=search["_id"], listings=listings)
@@ -48,7 +50,8 @@ def upsert_listings(search_id: ObjectId, listings: list = []):
     return True
 
 
-def get_listings(variables: dict = {}, offset: int = 0, limit: int = 1000, listings: list = []) -> list:
+@retry(wait_fixed=2000)
+def get_listings(search_id: ObjectId, variables: dict = {}, offset: int = 0, limit: int = 300, listings: list = []) -> list:
     """Get paginated Airbnb listings for search parameters"""
     variables["exploreRequest"]["itemsOffset"] = offset    
     
@@ -74,8 +77,8 @@ def get_listings(variables: dict = {}, offset: int = 0, limit: int = 1000, listi
     scraper = cloudscraper.create_scraper()
     r = scraper.get("https://www.airbnb.com/api/v3/ExploreSections", headers=headers, params=params)
     if r.status_code != 200:
-        print(r.text, r.status_code)
-        return listings
+        print(f"[{search_id}~{offset}] An error has occured, retrying...")
+        raise Exception()
     
     r = r.json()
     sections = r["data"]["presentation"]["explore"]["sections"]["sections"]
@@ -89,5 +92,6 @@ def get_listings(variables: dict = {}, offset: int = 0, limit: int = 1000, listi
     if len(listings) >= limit:
         return listings
     
+    time.sleep(2)
     offset = len(listings)
-    return get_listings(variables=variables, offset=offset, listings=listings)
+    return get_listings(search_id=search_id, variables=variables, offset=offset, listings=listings)
