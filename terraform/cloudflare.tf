4812a59c2e1dba4697320944feb58f3c819dc728
Nicolas Spehler <nicolas@spehler.com>
2026-09-21 20:27:46 +0800

    🔧 (terraform): Import the Email Routing MX records of realestatejobs.io and categoryapi.com
    Activating Email Routing added Cloudflare's MX records, each zone with its
    own hosts and priorities; they are imported as they are. The applied
    nicolas@spehler.com import block goes.

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -136,8 +136,3 @@ resource "cloudflare_dns_record" "google_search_console" {
 locals {
   cloudflare_email_dkim = "\"v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiweykoi+o48IOGuP7GR3X0MOExCUDY/BCRHoWBnh3rChl7WhdyCxW3jgq1daEjPPqoi7sJvdg5hEQVsgVRQP4DcnQDVjGMbASQtrY4WmB1VebF+RPJB2ECPsEDTpeiI5ZyUAwJaVX7r6bznU67g7LvFq35yIo4sdlmtZGV+i0H4cpYH9+3JJ78k\" \"m4KXwaf9xUJCWF6nxeD+qG6Fyruw1Qlbds2r85U9dkNDVAS3gioCvELryh1TxKGiVTkg4wqHTyHfWsp7KD3WQHYJn0RyfJJu6YEmL77zonn7p2SRMvTMP3ZEXibnC9gz3nnhR6wcYL8Q7zXypKTMD58bTixDSJwIDAQAB\""
 }
-
-import {
-  to = cloudflare_email_routing_address.nicolas
-  id = "04b947c14f54b8db6d507d04be69e1a9/d6fac7083b2247aba11cf49ad770bf79"
-}
--- terraform/cloudflare_domains.tf
+++ terraform/cloudflare_domains.tf
@@ -112,6 +112,36 @@ resource "cloudflare_email_routing_rule" "categoryapi_nicolas" {
   }]
 }
 
+resource "cloudflare_dns_record" "categoryapi_email_mx_1" {
+  zone_id  = cloudflare_zone.categoryapi.id
+  name     = "categoryapi.com"
+  content  = "route1.mx.cloudflare.net"
+  type     = "MX"
+  ttl      = 1
+  proxied  = false
+  priority = 52
+}
+
+resource "cloudflare_dns_record" "categoryapi_email_mx_2" {
+  zone_id  = cloudflare_zone.categoryapi.id
+  name     = "categoryapi.com"
+  content  = "route2.mx.cloudflare.net"
+  type     = "MX"
+  ttl      = 1
+  proxied  = false
+  priority = 91
+}
+
+resource "cloudflare_dns_record" "categoryapi_email_mx_3" {
+  zone_id  = cloudflare_zone.categoryapi.id
+  name     = "categoryapi.com"
+  content  = "route3.mx.cloudflare.net"
+  type     = "MX"
+  ttl      = 1
+  proxied  = false
+  priority = 12
+}
+
 resource "cloudflare_dns_record" "categoryapi_email_spf" {
   zone_id = cloudflare_zone.categoryapi.id
   name    = "categoryapi.com"
@@ -145,6 +175,36 @@ resource "cloudflare_email_routing_rule" "realestatejobs_nicolas" {
   }]
 }
 
+resource "cloudflare_dns_record" "realestatejobs_email_mx_1" {
+  zone_id  = cloudflare_zone.realestatejobs.id
+  name     = "realestatejobs.io"
+  content  = "amir.mx.cloudflare.net"
+  type     = "MX"
+  ttl      = 1
+  proxied  = false
+  priority = 7
+}
+
+resource "cloudflare_dns_record" "realestatejobs_email_mx_2" {
+  zone_id  = cloudflare_zone.realestatejobs.id
+  name     = "realestatejobs.io"
+  content  = "linda.mx.cloudflare.net"
+  type     = "MX"
+  ttl      = 1
+  proxied  = false
+  priority = 36
+}
+
+resource "cloudflare_dns_record" "realestatejobs_email_mx_3" {
+  zone_id  = cloudflare_zone.realestatejobs.id
+  name     = "realestatejobs.io"
+  content  = "isaac.mx.cloudflare.net"
+  type     = "MX"
+  ttl      = 1
+  proxied  = false
+  priority = 33
+}
+
 resource "cloudflare_dns_record" "realestatejobs_email_spf" {
   zone_id = cloudflare_zone.realestatejobs.id
   name    = "realestatejobs.io"
@@ -161,4 +221,34 @@ resource "cloudflare_dns_record" "realestatejobs_email_dkim" {
   type    = "TXT"
   ttl     = 1
   proxied = false
+}
+
+import {
+  to = cloudflare_dns_record.categoryapi_email_mx_1
+  id = "d88ab701190361b800e0514e40c4e229/7cdcadfb167347584e03a7b5e289a665"
+}
+
+import {
+  to = cloudflare_dns_record.categoryapi_email_mx_2
+  id = "d88ab701190361b800e0514e40c4e229/1fa22a2993214a4c0d53c27d55e5f17d"
+}
+
+import {
+  to = cloudflare_dns_record.categoryapi_email_mx_3
+  id = "d88ab701190361b800e0514e40c4e229/cc687903188d4d84e19baf5f3b1b94b4"
+}
+
+import {
+  to = cloudflare_dns_record.realestatejobs_email_mx_1
+  id = "e8be78c2590bf12e15694d9257aaebdc/a225b4fa7a2dd21285d9fd32ba2fba17"
+}
+
+import {
+  to = cloudflare_dns_record.realestatejobs_email_mx_2
+  id = "e8be78c2590bf12e15694d9257aaebdc/e63970bab070dd31cd431c052bda41a3"
+}
+
+import {
+  to = cloudflare_dns_record.realestatejobs_email_mx_3
+  id = "e8be78c2590bf12e15694d9257aaebdc/cb556be7466b73689fff8eaa2906836f"
 }
