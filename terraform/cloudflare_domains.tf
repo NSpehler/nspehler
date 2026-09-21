8790a84f880da0aa04e7d7395d1ff546a489d698
Nicolas Spehler <nicolas@spehler.com>
2026-09-21 20:51:05 +0800

    🔧 (terraform): Manage endless.engineer, leanmarketingforstartups.com and nicolasspehler.com
    The three zones and their records were only set up in the dashboard; they
    are imported here. Two of the redirect placeholders move from 129.0.2.1, a
    routable address typed by mistake, to the reserved 192.0.2.1.
    
    nicolasspehler.com's mail moves from ForwardEmail to Cloudflare Email
    Routing: a nicolas@ rule and a catch-all to nicolas@spehler.com, with
    Cloudflare's SPF and DKIM records. Its ForwardEmail records were deleted
    in the dashboard; the MX records Email Routing adds on activation get
    imported afterwards.

--- terraform/cloudflare_domains.tf
+++ terraform/cloudflare_domains.tf
@@ -22,6 +22,30 @@ resource "cloudflare_zone" "portalmonitor" {
   type = "full"
 }
 
+resource "cloudflare_zone" "endless" {
+  name = "endless.engineer"
+  account = {
+    id = var.cloudflare_account_id
+  }
+  type = "full"
+}
+
+resource "cloudflare_zone" "leanmarketingforstartups" {
+  name = "leanmarketingforstartups.com"
+  account = {
+    id = var.cloudflare_account_id
+  }
+  type = "full"
+}
+
+resource "cloudflare_zone" "nicolasspehler" {
+  name = "nicolasspehler.com"
+  account = {
+    id = var.cloudflare_account_id
+  }
+  type = "full"
+}
+
 # Cloudflare Email
 resource "cloudflare_email_routing_rule" "portalmonitor_nicolas" {
   zone_id = cloudflare_zone.portalmonitor.id
@@ -221,4 +245,163 @@ resource "cloudflare_dns_record" "realestatejobs_email_dkim" {
   type    = "TXT"
   ttl     = 1
   proxied = false
+}
+
+resource "cloudflare_email_routing_rule" "nicolasspehler_nicolas" {
+  zone_id = cloudflare_zone.nicolasspehler.id
+  name    = "nicolas@nicolasspehler.com"
+  enabled = true
+  matchers = [{
+    type  = "literal"
+    field = "to"
+    value = "nicolas@nicolasspehler.com"
+  }]
+  actions = [{
+    type  = "forward"
+    value = [cloudflare_email_routing_address.nicolas.email]
+  }]
+}
+
+resource "cloudflare_email_routing_catch_all" "nicolasspehler" {
+  zone_id  = cloudflare_zone.nicolasspehler.id
+  name     = "Catch-all"
+  enabled  = true
+  matchers = [{ type = "all" }]
+  actions = [{
+    type  = "forward"
+    value = [cloudflare_email_routing_address.nicolas.email]
+  }]
+}
+
+resource "cloudflare_dns_record" "nicolasspehler_email_spf" {
+  zone_id = cloudflare_zone.nicolasspehler.id
+  name    = "nicolasspehler.com"
+  content = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+  type    = "TXT"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "nicolasspehler_email_dkim" {
+  zone_id = cloudflare_zone.nicolasspehler.id
+  name    = "cf2024-1._domainkey.nicolasspehler.com"
+  content = local.cloudflare_email_dkim
+  type    = "TXT"
+  ttl     = 1
+  proxied = false
+}
+
+# Redirects
+resource "cloudflare_dns_record" "endless_apex" {
+  zone_id = cloudflare_zone.endless.id
+  name    = "endless.engineer"
+  content = "192.0.2.0"
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "leanmarketingforstartups_apex" {
+  zone_id = cloudflare_zone.leanmarketingforstartups.id
+  name    = "leanmarketingforstartups.com"
+  content = "192.0.2.1"
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "leanmarketingforstartups_www" {
+  zone_id = cloudflare_zone.leanmarketingforstartups.id
+  name    = "www.leanmarketingforstartups.com"
+  content = "leanmarketingforstartups.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "nicolasspehler_apex" {
+  zone_id = cloudflare_zone.nicolasspehler.id
+  name    = "nicolasspehler.com"
+  content = "192.0.2.1"
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "nicolasspehler_www" {
+  zone_id = cloudflare_zone.nicolasspehler.id
+  name    = "www.nicolasspehler.com"
+  content = "nicolasspehler.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+# Google Search Console
+resource "cloudflare_dns_record" "leanmarketingforstartups_google_search_console" {
+  zone_id = cloudflare_zone.leanmarketingforstartups.id
+  name    = "leanmarketingforstartups.com"
+  content = "google-site-verification=9f5V5xCWZ7rOZrcEpfTEWnRlym3Eq2gEfQdysFq9dMs"
+  type    = "TXT"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "nicolasspehler_google_search_console" {
+  zone_id = cloudflare_zone.nicolasspehler.id
+  name    = "nicolasspehler.com"
+  content = "google-site-verification=M1gQInE7vkBdbhEL-PgBXXXx_y6PMcEJPJ0GbrFdquA"
+  type    = "TXT"
+  ttl     = 1
+  proxied = false
+}
+
+import {
+  to = cloudflare_zone.nicolasspehler
+  id = "cdd84d7a0ad8da5ae170f11d96d184db"
+}
+
+import {
+  to = cloudflare_zone.leanmarketingforstartups
+  id = "0dd210718e6bec1373f9eb35a934a310"
+}
+
+import {
+  to = cloudflare_zone.endless
+  id = "1c280fd8b5e404575a2e83d9e522811a"
+}
+
+import {
+  to = cloudflare_dns_record.endless_apex
+  id = "1c280fd8b5e404575a2e83d9e522811a/87ed006b419076d97f3bb9bab14dc1d2"
+}
+
+import {
+  to = cloudflare_dns_record.leanmarketingforstartups_apex
+  id = "0dd210718e6bec1373f9eb35a934a310/2b95290af7fd680a126430268713a04d"
+}
+
+import {
+  to = cloudflare_dns_record.leanmarketingforstartups_www
+  id = "0dd210718e6bec1373f9eb35a934a310/8bbacb136b3d574a2d20a8d31029760a"
+}
+
+import {
+  to = cloudflare_dns_record.leanmarketingforstartups_google_search_console
+  id = "0dd210718e6bec1373f9eb35a934a310/1d93ed27d57b1c4fcc417ae0a520a343"
+}
+
+import {
+  to = cloudflare_dns_record.nicolasspehler_apex
+  id = "cdd84d7a0ad8da5ae170f11d96d184db/d66ff9fdc2ba82e4912a9f0ef407ab88"
+}
+
+import {
+  to = cloudflare_dns_record.nicolasspehler_www
+  id = "cdd84d7a0ad8da5ae170f11d96d184db/773cb8df1a567fe3bea390432be341f5"
+}
+
+import {
+  to = cloudflare_dns_record.nicolasspehler_google_search_console
+  id = "cdd84d7a0ad8da5ae170f11d96d184db/75da008d13e952e3158dc2bb64c49d4a"
 }
