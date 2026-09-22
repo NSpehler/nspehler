b36e44b5779b4cbe018d090b74b957ca8276643d
Nicolas Spehler <nicolas@spehler.com>
2026-09-22 09:50:55 +0800

    🔧 (terraform): Add the offtherecord/discogs-session secret

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -1,3 +1,7 @@
 resource "aws_secretsmanager_secret" "nspehler" {
   name = "nspehler/prod"
 }
+
+resource "aws_secretsmanager_secret" "discogs_session" {
+  name = "offtherecord/discogs-session"
+}
